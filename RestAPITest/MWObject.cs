d5c574d6e33495d2478f4621f369d99ab7cf5ecf
Spencer Harston <spencerharston@weber.edu>
2019-08-22 16:48:35 -0600

    Add classes; get a request

---
+++ RestAPITest/MWObject.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp.Deserializers;
+
+namespace RestAPITest
+{
+    public class Location
+    {
+        [DeserializeAs(Name = "title")]
+        public string Title { get; set; }
+
+        [DeserializeAs(Name = "location_type")]
+        public string LocationType { get; set; }
+
+        [DeserializeAs(Name = "latt_long")]
+        public string LattLong { get; set; }
+
+        [DeserializeAs(Name = "time")]
+        public DateTime Time { get; set; }
+
+        [DeserializeAs(Name = "sun_rise")]
+        public DateTime Sunrise { get; set; }
+
+        [DeserializeAs(Name = "sun_set")]
+        public DateTime Sunset { get; set; }
+
+        [DeserializeAs(Name = "timezone_name")]
+        public string TimezoneName { get; set; }
+
+        [DeserializeAs(Name = "woeid ")]
+        public int Woeid { get; set; }
+
+        [DeserializeAs(Name = "consolidated_weather")]
+        public List<ConsolidatedWeather> ConsolidatedWeather { get; set; }
+
+        [DeserializeAs(Name = "parent")]
+        public Parent Parent { get; set; }
+
+        [DeserializeAs(Name = "sources")]
+        public List<Source> Sources { get; set; }   
+    }
+
+    public class Parent
+    {
+        [DeserializeAs(Name = "title")]
+        public string Title { get; set; }
+
+        [DeserializeAs(Name = "location_type")]
+        public string LocationType { get; set; }
+
+        [DeserializeAs(Name = "latt_long")]
+        public string LattLong { get; set; }
+
+        [DeserializeAs(Name = "woeid")]
+        public int Woeid { get; set; }
+    }
+
+    public class Source
+    {
+        [DeserializeAs(Name = "title")]
+        public string Title { get; set; }
+
+        [DeserializeAs(Name = "url")]
+        public string URL { get; set; }
+
+        [DeserializeAs(Name = "slug")]
+        public string Slug { get; set; }
+
+        [DeserializeAs(Name = "crawl_rate")]
+        public int CrawlRate { get; set; }
+    }
+
+    public class ConsolidatedWeather
+    {
+        [DeserializeAs(Name = "id")]
+        public int ID { get; set; }
+
+        [DeserializeAs(Name = "applicable_date")]
+        public DateTime ApplicableDate { get; set; }
+
+        [DeserializeAs(Name = "weather_state_name")]
+        public string WeatherStateName { get; set; }
+
+        [DeserializeAs(Name = "weather_state_abbr")]
+        public string WeatherStateAbbreviation { get; set; }
+
+        [DeserializeAs(Name = "wind_speed")]
+        public float WindSpeed { get; set; }
+
+        [DeserializeAs(Name = "wind_direction")]
+        public float WindDirection { get; set; }
+
+        [DeserializeAs(Name = "wind_direction_compass")]
+        public string WindDirectionCompass { get; set; }
+
+        [DeserializeAs(Name = "min_temp")]
+        public int MinTemp { get; set; }
+
+        [DeserializeAs(Name = "max_temp")]
+        public int MaxTemp { get; set; }
+
+        [DeserializeAs(Name = "the_temp")]
+        public int Temp { get; set; }
+
+        [DeserializeAs(Name = "air_pressure")]
+        public float AirPressure { get; set; }
+
+        [DeserializeAs(Name = "humidity")]
+        public float Humidity { get; set; }
+
+        [DeserializeAs(Name = "visibility")]
+        public float Visibility { get; set; }
+
+        [DeserializeAs(Name = "predictability")]
+        public int Predictability { get; set; }
+    }
+}
--- RestAPITest/Program.cs
+++ RestAPITest/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Serialization;
+using Newtonsoft.Json;
 
 namespace RestAPITest
 {
@@ -10,6 +13,53 @@ namespace RestAPITest
     {
         static void Main(string[] args)
         {
+            var client = new RestClient("https://www.metaweather.com/api/")
+                .UseSerializer(() => new JsonNetSerializer());
+
+            var request = new RestRequest("location/2487610", DataFormat.Json);
+
+            Location location = new Location();
+            request.AddObject(location);
+
+            var response = client.Get(request);
+            var content = response.Content;
+
+            var response2 = client.Get<Location>(request);
+
+            Console.WriteLine(response.Content);
+
+            Console.WriteLine("---");
+
+            foreach(Source source in response2.Data.Sources)
+                Console.WriteLine("{0}: {1}", source.Title, source.URL);
+
+            Console.WriteLine("---");
+
+            //foreach(ConsolidatedWeather cw in response2.Data.ConsolidatedWeather)
+            //    Console.WriteLine("{0}: {1} C", cw.ApplicableDate, cw.Temp);
+
+            Console.ReadKey();
+        }
+
+        public class JsonNetSerializer : IRestSerializer
+        {
+            public string Serialize(object obj) =>
+            JsonConvert.SerializeObject(obj);
+
+            public string Serialize(Parameter parameter) =>
+                JsonConvert.SerializeObject(parameter.Value);
+
+            public T Deserialize<T>(IRestResponse response) =>
+                JsonConvert.DeserializeObject<T>(response.Content);
+
+            public string[] SupportedContentTypes { get; } =
+            {
+                "application/json", "text/json", "text/x-json", "text/javascript", "*+json"
+            };
+
+            public string ContentType { get; set; } = "application/json";
+
+            public DataFormat DataFormat { get; } = DataFormat.Json;
         }
     }
 }
