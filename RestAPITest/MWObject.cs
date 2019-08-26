1854099116a003355b74f63489f8dbbb342436b2
Spencer Harston <spencerharston@weber.edu>
2019-08-26 11:53:34 -0600

    Rewrite class properties to fit the response keys

--- RestAPITest/MWObject.cs
+++ RestAPITest/MWObject.cs
@@ -4,117 +4,171 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RestSharp.Deserializers;
+using Newtonsoft.Json.Serialization;
 
 namespace RestAPITest
 {
-    public class Location
+    public class ConsolidatedWeather
     {
-        [DeserializeAs(Name = "title")]
-        public string Title { get; set; }
-
-        [DeserializeAs(Name = "location_type")]
-        public string LocationType { get; set; }
-
-        [DeserializeAs(Name = "latt_long")]
-        public string LattLong { get; set; }
-
-        [DeserializeAs(Name = "time")]
-        public DateTime Time { get; set; }
-
-        [DeserializeAs(Name = "sun_rise")]
-        public DateTime Sunrise { get; set; }
-
-        [DeserializeAs(Name = "sun_set")]
-        public DateTime Sunset { get; set; }
-
-        [DeserializeAs(Name = "timezone_name")]
-        public string TimezoneName { get; set; }
-
-        [DeserializeAs(Name = "woeid ")]
-        public int Woeid { get; set; }
-
-        [DeserializeAs(Name = "consolidated_weather")]
-        public List<ConsolidatedWeather> ConsolidatedWeather { get; set; }
-
-        [DeserializeAs(Name = "parent")]
-        public Parent Parent { get; set; }
-
-        [DeserializeAs(Name = "sources")]
-        public List<Source> Sources { get; set; }   
+        public object ID { get; set; }
+        public string Weather_State_Name { get; set; }
+        public string Weather_State_Abbr { get; set; }
+        public string Wind_Direction_Compass { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Applicable_Date { get; set; }
+        public double Min_Temp { get; set; }
+        public double Max_Temp { get; set; }
+        public double The_Temp { get; set; }
+        public double Wind_Speed { get; set; }
+        public double Wind_Direction { get; set; }
+        public double Air_Pressure { get; set; }
+        public int Humidity { get; set; }
+        public double? Visibility { get; set; }
+        public int Predictability { get; set; }
     }
+
 
     public class Parent
     {
-        [DeserializeAs(Name = "title")]
         public string Title { get; set; }
-
-        [DeserializeAs(Name = "location_type")]
-        public string LocationType { get; set; }
-
-        [DeserializeAs(Name = "latt_long")]
-        public string LattLong { get; set; }
-
-        [DeserializeAs(Name = "woeid")]
+        public string Location_Type { get; set; }
         public int Woeid { get; set; }
+        public string Latt_Long { get; set; }
     }
 
     public class Source
     {
-        [DeserializeAs(Name = "title")]
         public string Title { get; set; }
-
-        [DeserializeAs(Name = "url")]
-        public string URL { get; set; }
-
-        [DeserializeAs(Name = "slug")]
         public string Slug { get; set; }
-
-        [DeserializeAs(Name = "crawl_rate")]
-        public int CrawlRate { get; set; }
+        public string URL { get; set; }
+        public int Crawl_Rate { get; set; }
     }
 
-    public class ConsolidatedWeather
+    public class Location
     {
-        [DeserializeAs(Name = "id")]
-        public int ID { get; set; }
-
-        [DeserializeAs(Name = "applicable_date")]
-        public DateTime ApplicableDate { get; set; }
-
-        [DeserializeAs(Name = "weather_state_name")]
-        public string WeatherStateName { get; set; }
-
-        [DeserializeAs(Name = "weather_state_abbr")]
-        public string WeatherStateAbbreviation { get; set; }
-
-        [DeserializeAs(Name = "wind_speed")]
-        public float WindSpeed { get; set; }
-
-        [DeserializeAs(Name = "wind_direction")]
-        public float WindDirection { get; set; }
-
-        [DeserializeAs(Name = "wind_direction_compass")]
-        public string WindDirectionCompass { get; set; }
-
-        [DeserializeAs(Name = "min_temp")]
-        public int MinTemp { get; set; }
-
-        [DeserializeAs(Name = "max_temp")]
-        public int MaxTemp { get; set; }
-
-        [DeserializeAs(Name = "the_temp")]
-        public int Temp { get; set; }
-
-        [DeserializeAs(Name = "air_pressure")]
-        public float AirPressure { get; set; }
-
-        [DeserializeAs(Name = "humidity")]
-        public float Humidity { get; set; }
-
-        [DeserializeAs(Name = "visibility")]
-        public float Visibility { get; set; }
-
-        [DeserializeAs(Name = "predictability")]
-        public int Predictability { get; set; }
+        public List<ConsolidatedWeather> Consolidated_Weather { get; set; }
+        public DateTime Time { get; set; }
+        public DateTime Sun_Rise { get; set; }
+        public DateTime Sun_Set { get; set; }
+        public string Timezone_Name { get; set; }
+        public Parent Parent { get; set; }
+        public List<Source> Sources { get; set; }
+        public string Title { get; set; }
+        public string Location_Type { get; set; }
+        public int Woeid { get; set; }
+        public string Latt_Long { get; set; }
+        public string Timezone { get; set; }
     }
 }
+
+//    public class Location
+//    {
+//        [DeserializeAs(Name = "title")]
+//        public string Title { get; set; }
+
+//        [DeserializeAs(Name = "location_type")]
+//        public string LocationType { get; set; }
+
+//        [DeserializeAs(Name = "latt_long")]
+//        public string LattLong { get; set; }
+
+//        [DeserializeAs(Name = "time")]
+//        public DateTime Time { get; set; }
+
+//        [DeserializeAs(Name = "sun_rise")]
+//        public DateTime Sunrise { get; set; }
+
+//        [DeserializeAs(Name = "sun_set")]
+//        public DateTime Sunset { get; set; }
+
+//        [DeserializeAs(Name = "timezone_name")]
+//        public string TimezoneName { get; set; }
+
+//        [DeserializeAs(Name = "woeid ")]
+//        public int Woeid { get; set; }
+
+//        [DeserializeAs(Name = "consolidated_weather")]
+//        public List<ConsolidatedWeather> ConsolidatedWeather { get; set; }
+
+//        [DeserializeAs(Name = "parent")]
+//        public Parent Parent { get; set; }
+
+//        [DeserializeAs(Name = "sources")]
+//        public List<Source> Sources { get; set; }   
+//    }
+
+//    public class Parent
+//    {
+//        [DeserializeAs(Name = "title")]
+//        public string Title { get; set; }
+
+//        [DeserializeAs(Name = "location_type")]
+//        public string LocationType { get; set; }
+
+//        [DeserializeAs(Name = "latt_long")]
+//        public string LattLong { get; set; }
+
+//        [DeserializeAs(Name = "woeid")]
+//        public int Woeid { get; set; }
+//    }
+
+//    public class Source
+//    {
+//        [DeserializeAs(Name = "title")]
+//        public string Title { get; set; }
+
+//        [DeserializeAs(Name = "url")]
+//        public string URL { get; set; }
+
+//        [DeserializeAs(Name = "slug")]
+//        public string Slug { get; set; }
+
+//        [DeserializeAs(Name = "crawl_rate")]
+//        public int CrawlRate { get; set; }
+//    }
+
+//    public class ConsolidatedWeather
+//    {
+//        [DeserializeAs(Name = "id")]
+//        public int ID { get; set; }
+
+//        [DeserializeAs(Name = "applicable_date")]
+//        public DateTime ApplicableDate { get; set; }
+
+//        [DeserializeAs(Name = "weather_state_name")]
+//        public string WeatherStateName { get; set; }
+
+//        [DeserializeAs(Name = "weather_state_abbr")]
+//        public string WeatherStateAbbreviation { get; set; }
+
+//        [DeserializeAs(Name = "wind_speed")]
+//        public float WindSpeed { get; set; }
+
+//        [DeserializeAs(Name = "wind_direction")]
+//        public float WindDirection { get; set; }
+
+//        [DeserializeAs(Name = "wind_direction_compass")]
+//        public string WindDirectionCompass { get; set; }
+
+//        [DeserializeAs(Name = "min_temp")]
+//        public int MinTemp { get; set; }
+
+//        [DeserializeAs(Name = "max_temp")]
+//        public int MaxTemp { get; set; }
+
+//        [DeserializeAs(Name = "the_temp")]
+//        public int Temp { get; set; }
+
+//        [DeserializeAs(Name = "air_pressure")]
+//        public float AirPressure { get; set; }
+
+//        [DeserializeAs(Name = "humidity")]
+//        public float Humidity { get; set; }
+
+//        [DeserializeAs(Name = "visibility")]
+//        public float Visibility { get; set; }
+
+//        [DeserializeAs(Name = "predictability")]
+//        public int Predictability { get; set; }
+//    }
+//}
--- RestAPITest/Program.cs
+++ RestAPITest/Program.cs
@@ -13,34 +13,42 @@ namespace RestAPITest
     {
         static void Main(string[] args)
         {
+            //create the rest client, along with a custom serializer
             var client = new RestClient("https://www.metaweather.com/api/")
                 .UseSerializer(() => new JsonNetSerializer());
 
+            //create the request
             var request = new RestRequest("location/2487610", DataFormat.Json);
 
-            Location location = new Location();
-            request.AddObject(location);
+            //Location location = new Location();
+            //request.AddObject(location); //for POSTing
+            
+            //Deserialize the request to an object along with the response
+            var response = client.Get<Location>(request);
 
-            var response = client.Get(request);
-            var content = response.Content;
-
-            var response2 = client.Get<Location>(request);
-
-            Console.WriteLine(response.Content);
-
-            Console.WriteLine("---");
-
-            foreach(Source source in response2.Data.Sources)
-                Console.WriteLine("{0}: {1}", source.Title, source.URL);
+            //foreach (Source source in response.Data.Sources)
+            //    Console.WriteLine("{0}: {1}", source.Title, source.URL);
 
             Console.WriteLine("---");
 
-            //foreach(ConsolidatedWeather cw in response2.Data.ConsolidatedWeather)
-            //    Console.WriteLine("{0}: {1} C", cw.ApplicableDate, cw.Temp);
+            Console.WriteLine("Date\t\tMin\t\tMax\t\tState");
+            foreach (ConsolidatedWeather cw in response.Data.Consolidated_Weather)
+            {
+                Console.WriteLine("{0}\t{1} F\t{2} F\t{3}", cw.Applicable_Date.ToShortDateString(), ConvertTemp.CtoF(cw.Min_Temp).ToString().PadRight(6, '0'), ConvertTemp.CtoF(cw.Max_Temp).ToString().PadRight(6, '0'), cw.Weather_State_Name);
+            }
+
+            Console.WriteLine("---");
+
+            var request2 = new RestRequest("location/2487610/2019/8/25", DataFormat.Json);
+            var response2 = client.Execute<List<ConsolidatedWeather>>(request2);
+
 
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// Custom serializer using NewtonSoft's JSON serializer
+        /// </summary>
         public class JsonNetSerializer : IRestSerializer
         {
             public string Serialize(object obj) =>
@@ -61,5 +69,40 @@ namespace RestAPITest
 
             public DataFormat DataFormat { get; } = DataFormat.Json;
         }
+
+        /// <summary>
+        /// Class to convert temperatures
+        /// </summary>
+        static class ConvertTemp
+        {
+            /// <summary>
+            /// Convert Celsius to Farenheit
+            /// </summary>
+            /// <param name="temp">Temperature in celsius</param>
+            /// <returns>Temperature in farenheit</returns>
+            public static double CtoF(double temp) => (9.0 / 5.0 * temp) + 32;
+
+            /// <summary>
+            /// Convert Farenheit to Celsius
+            /// </summary>
+            /// <param name="temp">Temperature in Farenheit</param>
+            /// <returns>Temperature in celsius</returns>
+            public static double FtoC(double temp) => 5.0 / 9.0 * (temp - 32);
+
+            /// <summary>
+            /// Converts celsius to kelvin
+            /// </summary>
+            /// <param name="temp">Temperature in celsius</param>
+            /// <returns>Temperature in kelvin</returns>
+            public static double CtoK(double temp) => FtoK(CtoF(temp));
+
+            /// <summary>
+            /// Converts farenheit to kelvin
+            /// </summary>
+            /// <param name="temp">Temperature in farenheit</param>
+            /// <returns>Temperature in kelvin</returns>
+            public static double FtoK(double temp) => ((temp - 32) * (5.0 / 9.0)) + 273.15;
+        }
+
     }
 }
