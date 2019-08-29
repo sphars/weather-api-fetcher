f06d4432375bce20a18852960bae0cffc90e0b46
Spencer Harston <spencer314@gmail.com>
2019-08-29 15:04:16 -0600

    Add settings to fetch API keys from apikeys.config

--- RestAPITest/Program.cs
+++ RestAPITest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 using RestSharp;
 using RestSharp.Serialization;
 using Newtonsoft.Json;
